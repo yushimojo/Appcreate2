4b7b284e7b62ad76bf9e99dc41a9267b43ab812e
YuShimojo <yongxiatiao@gmail.com>
2022-06-25 13:52:48 +0000

    [add]サインアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do 
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
+  
   
   get 'home/about' => 'homes#about', as:'about'
  
  resources :users,only:[:show,:edit,:index] 
+ resources :books,only:[:show,:edit,:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
